f2d55a898995659267894d958129bfda4076bf6d
ScottieCornett <scottie.cornett@gmail.com>
2023-07-06 18:00:23 -0400

    Application DbContext configured for SQLite and db created

---
+++ JimazonLite.Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using JimazonLite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JimazonLite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public string DbPath { get; }
+
+        public ApplicationDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "JimazonLite.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
