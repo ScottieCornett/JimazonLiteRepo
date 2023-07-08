42bcf96555f8d738ab6279e4b4d7b3b0756b3785
ScottieCornett <scottie.cornett@gmail.com>
2023-07-08 14:49:51 -0400

    generic repository implemented

--- JimazonLite.Data/Repository/IRepository/IRepository.cs
+++ JimazonLite.Data/Repository/IRepository/IRepository.cs
@@ -12,8 +12,7 @@ namespace JimazonLite.Data.Repository.IRepository
         IEnumerable<T> GetAll();
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
-        void Update(T entity);
-        void Delete(T entity);
-        void DeleteRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
---
+++ JimazonLite.Data/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using JimazonLite.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JimazonLite.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> dbSet; 
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            this.dbSet = dbContext.Set<T>();
+            // _db.Products == dbSet
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
