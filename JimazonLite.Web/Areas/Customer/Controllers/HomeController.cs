36f4ff86680b538f33c221bbd3656b52b5ec2f69
ScottieCornett <scottie.cornett@gmail.com>
2023-07-10 22:40:27 -0400

    added front end styling

--- JimazonLite.Web/Areas/Customer/Controllers/HomeController.cs
+++ JimazonLite.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using JimazonLite.Models;
+﻿using JimazonLite.Data.Repository.IRepository;
+using JimazonLite.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,18 @@ namespace JimazonLite.Web.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(products);
         }
 
         public IActionResult Privacy()
