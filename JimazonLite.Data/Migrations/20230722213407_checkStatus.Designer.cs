6bec57f8f4d9ef89390fb55e831356ed13c0fb79
ScottieCornett <scottie.cornett@gmail.com>
2023-07-22 17:35:17 -0400

    updated product data

--- JimazonLite.Data/Migrations/20230722213407_checkStatus.Designer.cs
+++ JimazonLite.Data/Migrations/20230722213407_checkStatus.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JimazonLite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JimazonLite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230722213407_checkStatus")]
+    partial class checkStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
