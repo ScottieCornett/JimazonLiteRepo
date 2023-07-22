e79a868cbd005e2289afab13dc0f56c9f15b54fc
ScottieCornett <scottie.cornett@gmail.com>
2023-07-22 19:34:19 -0400

    update seed table

--- JimazonLite.Data/ApplicationDbContext.cs
+++ JimazonLite.Data/ApplicationDbContext.cs
@@ -34,10 +34,13 @@ namespace JimazonLite.Data
 
             modelBuilder.Entity<Product>().HasData(
                 new Product { Id = 1, Name = "Electric Guitar", Description = "Donner Electric Guitar", ModelNumber = "DT400", Price = 125.00f, ImageUrl = @"images\product\acab30e9-3a78-4483-ac1a-257649072f17.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 5},
-                new Product { Id = 2, Name = "Studio Speaker", Description = "Yamaha Studio Speaker", ModelNumber = "HS7", Price = 150.00f, ImageUrl = "", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 1 },
-                new Product { Id = 3, Name = "Camping Cot", Description = "Folding Camping Cot", ModelNumber = "", Price = 50.00f, ImageUrl = "", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 3 },
-                new Product { Id = 4, Name = "Bicycle", Description = "Koda Retrospec Bicycle", ModelNumber = "", Price = 40.00f, ImageUrl = "", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 3 },
-                new Product { Id = 5, Name = "Hedge Trimmer", Description = "GreenWorks 24V Hedge Trimmer", ModelNumber = "", Price = 35.00f, ImageUrl = "", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 4 });
+                new Product { Id = 2, Name = "Studio Speaker", Description = "Yamaha Studio Speaker", ModelNumber = "HS7", Price = 150.00f, ImageUrl = @"images\product\a72415bc-2812-42e6-94a2-98da457c01fa.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 1 },
+                new Product { Id = 3, Name = "ASUS ROG RTX 3080Ti", Description = "Asus Republic of Gamers Nvidia RTX 3080Ti 12GB", ModelNumber = "", Price = 800f, ImageUrl = @"images\product\7e11a6a3-805a-471f-a0c9-a7d1dd6d0db7.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 1 },
+                new Product { Id = 4, Name = "Bicycle", Description = "Koda Retrospec Bicycle", ModelNumber = "KODA14A", Price = 40.00f, ImageUrl = @"images\product\c459486b-f50a-4841-8f84-2a52914e6f74.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 3 },
+                new Product { Id = 5, Name = "Hedge Trimmer", Description = "GreenWorks 24V Hedge Trimmer", ModelNumber = "GRNW9687", Price = 35.00f, ImageUrl = @"images\product\35f44e1d-e97a-4470-a9da-5c6931c64138.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 4 },
+                new Product { Id = 6, Name = "Flight Stick", Description = "Thrustmaster T-Hotas Flight Stick for Xbox/PX", ModelNumber = "THR5521", Price = 90.00f,ImageUrl = @"images\product\2f6ebfc9-49f0-4e30-b28a-f3e3ef4d982b.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 1 },
+                new Product { Id = 7, Name = "Xbox Series X", Description = "Xbox Series X 1TB Video Game Console", ModelNumber = "MCSFTXBX901A", Price = 400.00f, ImageUrl = @"images\product\3ce1528f-273e-440d-855f-e54ab679cb95.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 1 },
+                new Product { Id = 8, Name = "MSI RTX 4090", Description = "MSI Gaming Trio Nvidia RTX 4900 24BG", ModelNumber = "MSI4090", Price = 1350.00f ,ImageUrl = @"images\product\d3ecb2fa-5216-4f55-b2a5-dce5feead3f0.jpg", DateAdded = DateTime.Today, Quantity = 1, CategoryId = 1 });
 
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Electronics"},
--- JimazonLite.Data/Migrations/20230722233329_updateSeedTable.Designer.cs
+++ JimazonLite.Data/Migrations/20230722233329_updateSeedTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JimazonLite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JimazonLite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230722233329_updateSeedTable")]
+    partial class updateSeedTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
