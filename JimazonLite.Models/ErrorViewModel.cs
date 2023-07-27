4b5afa4591b25fe95d8cd2d43ee3c390bbef7ced
ScottieCornett <scottie.cornett@gmail.com>
2023-07-27 19:23:16 -0400

    cleaning cache

--- JimazonLite.Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JimazonLite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
